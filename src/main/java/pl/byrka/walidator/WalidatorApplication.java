b4e595f6e7e60e8dd23ab8599a45f7338c85c58a
Łukasz Byrka <lukasz.byrka1@gmail.com>
2022-04-14 21:05:19 +0200

    Initial commit

---
+++ src/main/java/pl/byrka/walidator/WalidatorApplication.java
@@ -0,0 +1,13 @@
+package pl.byrka.walidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/byrka/walidator/WalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.byrka.walidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
