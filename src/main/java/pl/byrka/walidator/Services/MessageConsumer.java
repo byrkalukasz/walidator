76562e18a5a032b6c4ee3f26769b12bf3286eefa
Łukasz Byrka <lukasz.byrka1@gmail.com>
2022-04-15 19:57:04 +0200

    implementing gson

--- src/main/java/pl/byrka/walidator/Services/MessageConsumer.java
+++ src/main/java/pl/byrka/walidator/Services/MessageConsumer.java
@@ -1,5 +1,6 @@
 package pl.byrka.walidator.Services;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
@@ -12,10 +13,14 @@ import javax.sound.midi.SysexMessage;
 public class MessageConsumer {
     @Autowired
     private JmsTemplate jmsTemplate;
+    @Autowired
+    private Gson gson;
 
     @JmsListener(destination = "${activemq.receive}")
     public void messagelistener(String application){
         System.out.println(application);
+        var model = gson.fromJson(application,StudentApplication.class);
+        System.out.println(model.toString());
         messageSent(application);
     }
 
